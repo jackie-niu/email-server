45693d4978ba171650fca17f659b62b84362ea13
Jackie Niu <jackie.niu10@gmail.com>
2021-01-16 16:32:21 -0500

    Delete Init.java

--- Init.java
+++
@@ -1,7 +0,0 @@
-public class Init {
-    public static void initializeBoxMessages() {
-	for (int i = 0; i < Globals.boxMessages.length; i++) {
-	    Globals.boxMessages[i] = Globals.EMPTY_CLIENT_MESSAGE;
-	}
-   }    
-}
